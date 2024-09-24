ef78d2850ccc8629f26cde7b6e901c197251dbf8
Your NameAhsanHabib002 <ahsanhabib12300@gmail.com>
2024-09-24 18:53:02 +0600

    toggle done

--- js/calculation.js
+++ js/calculation.js
@@ -20,3 +20,73 @@ document.getElementById('donate-btn1')
             alert('Not enough money')
         }
     })
+// d-2
+
+document.getElementById('donate-btn2')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        const donateMoney = getInputValuById('donate-money2');
+        
+        const balance = getTextValuById('account-balance');
+
+        const donation = getTextValuById('get-money2')
+        if(donateMoney <= balance){
+            const newBalance = balance - donateMoney;
+
+            const donateBalance = donation + donateMoney;
+
+            document.getElementById('account-balance').innerText = newBalance;
+            document.getElementById('get-money2').innerText = donateBalance;
+        }
+
+        else{
+            alert('Not enough money')
+        }
+    })
+
+// d-3
+
+document.getElementById('donate-btn3')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        const donateMoney = getInputValuById('donate-money3');
+        
+        const balance = getTextValuById('account-balance');
+
+        const donation = getTextValuById('get-money3')
+        if(donateMoney <= balance){
+            const newBalance = balance - donateMoney;
+
+            const donateBalance = donation + donateMoney;
+
+            document.getElementById('account-balance').innerText = newBalance;
+            document.getElementById('get-money3').innerText = donateBalance;
+        }
+
+        else{
+            alert('Not enough money')
+        }
+    })
+
+// toggle
+
+document.getElementById('tg-donate')
+    .addEventListener('click', function(){
+        showSectionById('donate')
+    })
+
+    document.getElementById('tg-history')
+    .addEventListener('click', function(){
+        showSectionById('history')
+    })
+
+// active
+document.getElementById('tg-donate')
+    .addEventListener('click', function(){
+        activeMode('tg-donate')
+    })
+
+    document.getElementById('tg-history')
+    .addEventListener('click', function(){
+        activeMode('tg-history')
+    })
--- js/library.js
+++ js/library.js
@@ -11,3 +11,23 @@ function getTextValuById(id){
     const textMoneyNumber = parseFloat(textValue)
     return textMoneyNumber;
 }
+
+
+// toggle
+
+function showSectionById(id){
+    document.getElementById('donate').classList.add('hidden');
+    document.getElementById('history').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+
+// color change
+
+function activeMode(id){
+    document.getElementById('tg-donate').classList.remove('bg-[#B4F461]');
+    document.getElementById('tg-donate').classList.add('btn-outline');
+    document.getElementById('tg-history').classList.remove('bg-[#B4F461]');
+    document.getElementById('tg-history').classList.add('btn-outline');
+    document.getElementById(id).classList.add('bg-[#B4F461]');
+    document.getElementById(id).classList.remove('btn-outline');
+}
